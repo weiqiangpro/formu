6e896a3ea3fb865eede14298e82a1fce1e8d1c5d
weiqiangpro <280803590@qq.com>
2019-09-30 22:44:09 +0800

    Initial commit

---
+++ web/src/main/java/com/formu/FormuApplication.java
@@ -0,0 +1,13 @@
+package com.formu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormuApplication.class, args);
+    }
+
+}
